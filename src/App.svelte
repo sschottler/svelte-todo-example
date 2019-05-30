790b4279e1af7997ac625497914e0042990fbe16
Scott Schottler <scott.schottler@rallyhealth.com>
2019-05-30 12:53:47 -0400

    added functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,97 @@
 <script>
+  import { onMount } from "svelte";
+  import { fade, fly } from "svelte/transition";
 
+  const ENTER_KEY = 13;
+  const ESCAPE_KEY = 27;
+
+  let currentFilter = "all";
+  let newTodo = "";
+  let tempId = 4;
+  let todos = [
+    {
+      id: 1,
+      completed: true,
+      title: "Build Svelte Todo List",
+      editing: false
+    },
+    {
+      id: 2,
+      completed: false,
+      title: "Take Svelte Udemy course",
+      editing: false
+    },
+    {
+      id: 3,
+      completed: false,
+      title: "Read Svelte docs",
+      editing: false
+    }
+  ];
+
+  function addTodo(event) {
+    if (event.which === ENTER_KEY) {
+      todos = [
+        ...todos,
+        {
+          title: newTodo,
+          id: tempId,
+          completed: false,
+          editing: false
+        }
+      ];
+
+      tempId++;
+      newTodo = "";
+    }
+  }
+
+  function editTodo(todo) {
+    todo.editing = true;
+    todos = todos;
+  }
+
+  function doneEdit(todo) {
+    todo.editing = false;
+    todos = todos;
+  }
+
+  function doneEditKeydown(todo, event) {
+    if (event.which === ENTER_KEY) {
+      doneEdit(todo);
+    }
+  }
+
+  function deleteTodo(id) {
+    todos = todos.filter(todo => todo.id !== id);
+  }
+
+  function checkAllTodos(event) {
+    todos.forEach(todo => (todo.completed = event.target.checked));
+    todos = todos;
+  }
+
+  function clearCompleted(event) {
+    todos = todos.filter(todo => !todo.completed);
+  }
+
+  function updateFilter(filter) {
+    currentFilter = filter;
+  }
+
+  onMount(async () => {
+    const res = await fetch("https://api.kanye.rest");
+    const response = await res.json();
+    console.log(response.quote);
+  });
+
+  $: todosRemaining = todos.filter(todo => !todo.completed).length;
+  $: filteredTodos =
+    currentFilter === "all"
+      ? todos
+      : currentFilter === "completed"
+      ? todos.filter(todo => todo.completed)
+      : todos.filter(todo => !todo.completed);
 </script>
 
 <style lang="scss">
@@ -91,49 +183,69 @@
 <div class="container">
   <img src={'/img/svelte-logo-horizontal.svg'} alt="svelte logo" class="logo" />
 
-  <input type="text" class="todo-input" placeholder="What needs to be done" />
+  <input
+    type="text"
+    class="todo-input"
+    bind:value={newTodo}
+    on:keydown={addTodo}
+    placeholder="What needs to be done" />
 
-  <div class="todo-item">
-    <div class="todo-item-left">
-      <input type="checkbox" />
-      <div class="todo-item-label">Todo Title</div>
+  {#each filteredTodos as todo}
+    <div class="todo-item">
+      <div class="todo-item-left" transition:fly={{ y: 20, duration: 300 }}>
+        <input type="checkbox" bind:checked={todo.completed} />
+        {#if !todo.editing}
+          <div
+            class="todo-item-label"
+            class:completed={todo.completed}
+            on:dblclick={() => editTodo(todo)}>
+             {todo.title}
+          </div>
+        {:else}
+          <input
+            type="text"
+            bind:value={todo.title}
+            on:blur={() => doneEdit(todo)}
+            on:keydown={() => doneEditKeydown(todo, event)}
+            class="todo-item-edit"
+            autofocus />
+        {/if}
+      </div>
+      <div class="remove-item" on:click={() => deleteTodo(todo.id)}>×</div>
     </div>
-    <div class="remove-item">×</div>
-  </div>
-  <div class="todo-item">
-    <div class="todo-item-left">
-      <input type="checkbox" v-model="todo.completed" />
-      <div class="todo-item-label">Todo Title</div>
-    </div>
-    <div class="remove-item">×</div>
-  </div>
-  <div class="todo-item">
-    <div class="todo-item-left">
-      <input type="checkbox" v-model="todo.completed" />
-      <div class="todo-item-label">Todo Title</div>
-    </div>
-    <div class="remove-item">×</div>
-  </div>
+  {/each}
 
   <div class="extra-container">
     <div>
       <label>
-        <input type="checkbox" />
+        <input type="checkbox" on:change={checkAllTodos} />
         Check All
       </label>
     </div>
-    <div>3 items left</div>
+    <div>{todosRemaining} items left</div>
   </div>
 
   <div class="extra-container">
     <div>
-      <button>All</button>
-      <button>Active</button>
-      <button>Completed</button>
+      <button
+        on:click={() => updateFilter('all')}
+        class:active={currentFilter === 'all'}>
+        All
+      </button>
+      <button
+        on:click={() => updateFilter('active')}
+        class:active={currentFilter === 'active'}>
+        Active
+      </button>
+      <button
+        on:click={() => updateFilter('completed')}
+        class:active={currentFilter === 'completed'}>
+        Completed
+      </button>
     </div>
 
     <div>
-      <button>Clear Completed</button>
+      <button on:click={clearCompleted}>Clear Completed</button>
     </div>
   </div>
 </div>
